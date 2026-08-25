32f4db0d3a511b9169576116e868f1f5ffc26600
Ben Saunders-Henning <ben.shp@proton.me>
2026-08-25 19:46:17 -0400

    Update form fields and invoked update_config on valid submit

--- jstg/src/views/Settings.vue
+++ jstg/src/views/Settings.vue
@@ -22,26 +22,32 @@
     // form schema
     const { errors, handleSubmit, setFieldError, defineField, setFieldValue } = useForm({
         validationSchema: toTypedSchema(z.object({
-            documentSavePath: z.string().trim().min(1),
+            user_config: z.object({
+                document_save_path: z.string().trim().min(1),
+            }),
+            advanced: z.object({
+                document_api_url: z.string().trim().min(1),
+            }),
+
         }))
     });
 
     // fields
-    const [documentSavePath, documentSavePathAtrb] = defineField("documentSavePath");
-    const [documentApiURL, documentApiURLAtrb] = defineField("documentApiURL");
+    const [documentSavePath, documentSavePathAtrb] = defineField("user_config.document_save_path");
+    const [documentApiURL, documentApiURLAtrb] = defineField("advanced.document_api_url");
 
     const onSubmit = handleSubmit(onSuccess, onInvalidSubmit);
 
     function onSuccess(values) {
 
-        // invoke('verify_directory', { directory: values.documentSavePath }).then((truthy) => {
-        //     if(truthy) {
-        //         invoke('update_config', { conf: values });
-        //     }
-        //     else {
-        //         setFieldError('documentSavePath', 'Not a valid path');
-        //     }
-        // })
+        invoke('verify_directory', { directory: values.user_config.document_save_path }).then((truthy) => {
+            if(truthy) {
+                invoke('update_config', { config: values });
+            }
+            else {
+                setFieldError('user_config.document_save_path', 'Not a valid path');
+            }
+        })
 
     }
 
@@ -49,14 +55,14 @@
         console.log(errors);
     }
 
-    onMounted(() => {
-
-        invoke('get_config').then((init_config) => {
-            setFieldValue('documentSavePath', init_config.user_config.document_save_path);
-            setFieldValue('documentApiURL', init_config.advanced.document_api_url);
-        })
-        .catch((e) => console.log(e));
-
+    onMounted(async () => {
+        try {
+            const config = await invoke<Config>('get_config');
+            setFieldValue('user_config.document_save_path', config.user_config.document_save_path);
+            setFieldValue('advanced.document_api_url', config.advanced.document_api_url);
+        } catch (e) {
+            console.log(e)
+        }
     })
 
 </script>
@@ -68,14 +74,14 @@
             <div class="path-input vertical-input">
                 <p class="input-label">Document Save Path</p>
                 <input style="width: auto" aria-label="Document Save Path" id="savepath-input" class="input-border" type="text" name="savepath" v-model="documentSavePath" :="documentSavePathAtrb"/>
-                <span class="error">{{errors['documentSavePath']}}</span>
+                <span class="error">{{errors['user_config.document_save_path']}}</span>
             </div>
         </div>
         <div class="inputs">
             <div class="path-input vertical-input">
                 <p class="input-label">Document API URL</p>
                 <input style="width: auto" aria-label="Document API URL" id="document-api-input" class="input-border" type="text" name="documentApiURL" v-model="documentApiURL" :="documentApiURLAtrb"/>
-                <span class="error">{{errors['documentApiURL']}}</span>
+                <span class="error">{{errors['advanced.document_api_url']}}</span>
             </div>
         </div>
         <div class="horizontal-input" style="justify-content: center; margin-top: 30px;">
